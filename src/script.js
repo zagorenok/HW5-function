fde2790b3b280efba94a25c31f93fe96ef8e64cf
Victoria Ostapchuk <zagorenok@gmail.com>
2022-04-08 13:36:10 +0300

    changes

--- src/script.js
+++ src/script.js
@@ -1,35 +1,43 @@
 console.log('-------#4 Упрощенный вариант');
 
-function newNumbers(A, B) {
-  if ( A > B) {
+function newNumbers(a, b) {
+  if ( a > b) {
     return;
   }
-  console.log(A);
-  newNumbers(A + 1, B);
+  console.log(a);
+  newNumbers(a + 1, b);
 }
 newNumbers(3, 9);
 
 console.log('-------#4 Усложненный вариант');
 
-function anotherNumbers(A, B) {
-  console.log(A);
-  if (A < B) {
-    A++;
-  } else if (A > B) {
-    A--;
+function anotherNumbers(a, b) {
+  console.log(a);
+  if (a < b) {
+    a++;
+  } else if (a > b) {
+    a--;
   } else {
     return;
   }
-  anotherNumbers(A, B);
+  anotherNumbers(a, b);
 }
 anotherNumbers(9, 3);
 
 console.log('--------#5 Реализовать часы');
 
-let data = new Date();
-let hour = data.getHours();
-let minutes = data.getMinutes();
-let seconds = data.getSeconds();
-alert('Текущее время:' + hour + ':' + minutes + ':' + seconds);
+const clock = document.querySelector('#clock');
+let formatFull = true;
+let time = '';
+
+setInterval(function() {
+  if (formatFull) {
+      time = new Date().toTimeString().split(' ')[0];
+  } else {
+      time = new Date().toTimeString().split(' ')[0].substring(0,5); // то же, что и выше + substring(0,5) обрезаем двоеточие с секундами
+  }
+  clock.innerHTML = time;
+}, 1000);
+
 
 
